869d4d2deb01d1c4c604ad941bd8781e14b10abd
yuhangzhao <yuhangzhao@163.com>
2021-07-09 10:03:27 +0800

    增加了PowerBankService

---
+++ src/com/ssdut/spbs/service/Impl/PowerBankServiceImpl.java
@@ -0,0 +1,9 @@
+package com.ssdut.spbs.service.Impl;
+
+import com.ssdut.spbs.service.PowerBankService;
+
+public class PowerBankServiceImpl implements PowerBankService {
+
+
+
+}
---
+++ src/com/ssdut/spbs/service/PowerBankService.java
@@ -0,0 +1,8 @@
+package com.ssdut.spbs.service;
+import com.ssdut.spbs.entity.*;
+import java.util.*;
+public interface PowerBankService {
+
+
+
+}
