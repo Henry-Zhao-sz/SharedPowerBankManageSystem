6cd5b0f4ca9e832aa84f323009930b2e1a2ea3a2
Jty-123 <m911381@126.com>
2021-07-10 21:32:29 +0800

    增加根据用户ID查询用户所有订单的功能

--- src/com/ssdut/spbs/dao/impl/orderDaoImpl.java
+++ src/com/ssdut/spbs/dao/impl/orderDaoImpl.java
@@ -1,17 +1,20 @@
 package com.ssdut.spbs.dao.impl;
 
 import com.ssdut.spbs.dao.orderDao;
+import com.ssdut.spbs.entity.loc;
 import com.ssdut.spbs.entity.order;
 import com.ssdut.spbs.util.JdbcUtil;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class orderDaoImpl implements orderDao {
 
     @Override
-    public order queryByOrderID(int orderID) {
+    public order queryByOrderID(int orderID) {//根据订单号查询订单
         // TODO Auto-generated method stub
         Connection conn = null;
         PreparedStatement st = null;
@@ -45,5 +48,44 @@ public class orderDaoImpl implements orderDao {
         }
         return order1;//返回查询出的结果对象
     }
+    @Override
+    public List<order> queryByUserID(int orderUserID)//查询单个用户所有订单
+    {
+        // TODO Auto-generated method stub
+        Connection conn = null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        order order1=null;
+        List <order> list=new ArrayList<order>();
+        try {
+            conn = JdbcUtil.getConnection();//连接数据库
+            //写出sql语句，将可变变量，用？做替代，进行预编译
+            st = conn.prepareStatement("select * from order where `orderUserID=?" );
+            //将参数进行替换
+            st.setInt(1, orderUserID);
+            //System.out.println(st.toString());
+            rs=st.executeQuery();//执行语句
+            while(rs.next()) {//遍历查询结果
+                order1=new order();
+                order1.setOrderID(rs.getInt("orderID"));
+                order1.setOrderUserID(rs.getInt("orderUserID"));
+                order1.setOrderLendLocID(rs.getString("orderLendLocID"));
+                order1.setOrderPbID(rs.getInt("orderPbID"));
+                order1.setOrderCreateTime(rs.getDate("orderCreateTime"));
+                order1.setOrderHasFinished(rs.getInt("orderHasFinished"));
+                order1.setOrderLendLocID(rs.getString("orderLendLocID"));
+                order1.setOrderRevertLocID(rs.getString("orderRevertLocID"));
+                order1.setOrderFinishTime(rs.getDate("orderFinishTime"));
+                order1.setOrderCost(rs.getDouble("orderCost"));
+                list.add(order1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JdbcUtil.closeAll(rs, st, conn);//关闭连接
+        }
+        return list;//返回该用户的所有的订单的列表
     }
+}
+
 
--- src/com/ssdut/spbs/dao/orderDao.java
+++ src/com/ssdut/spbs/dao/orderDao.java
@@ -2,6 +2,8 @@ package com.ssdut.spbs.dao;
 
 import com.ssdut.spbs.entity.*;
 
+import java.util.List;
+
 public interface orderDao {
     /**
      * 根据订单ID查询订单信息
@@ -9,4 +11,11 @@ public interface orderDao {
      * @return 数据表中有一条记录返回一个order对象，否则返回null
      */
      order queryByOrderID(int orderID);
+    /**
+     * 根据用户ID查询用户订单信息
+     * @param orderUserID
+     * @return 数据表中有一条记录返回一个order对象，否则返回null
+     */
+    List<order> queryByUserID(int orderUserID);
+
 }
